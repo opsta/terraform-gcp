935c0d7c10930c684cfe8daddf361d42b198c07e
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2020-06-10 12:18:06 +0700

    fix bug from ip_allocation_policy

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -1,50 +1,11 @@
 variable "project_id" {
   description = "project id to provision GKE"
   type        = string
+  default     = null
 }
 
 variable "region" {
   description = "region to create VPC"
-  default     = "asia-southeast1"
   type        = string
+  default     = null
 }
-
-variable "zone" {
-  description = "zone of GKE cluster [a, b, c] (empty for enable multi zone cluster)"
-  default     = ""
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "name of GKE cluster to provision"
-  type        = string
-  default     = ""
-}
-
-variable "gke_username" {
-  description = "GKE username"
-  type        = string
-  default     = ""
-}
-
-variable "gke_password" {
-  description = "GKE password"
-  type        = string
-  default     = ""
-}
-
-variable "node_count" {
-  description = "number of GKE nodes per zone"
-  type        = number
-  default     = 1
-}
-
-variable "machine_type" {
-  description = "type of machine to run as workers"
-  type        = string
-  default     = "n1-standard-1"
-}
-
-/// TODO: Add more variables to support all values of GKE module 
-// preemptible
-// all
