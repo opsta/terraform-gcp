f3f076916228ebb771661fcab65190de4f1db99d
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2020-08-06 11:37:31 +0700

    add sql module example

--- examples/sql/main.tf
+++ examples/sql/main.tf
@@ -1,11 +1,33 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name = "example-vpc"
+  subnets = [
+    {
+      name                     = "example-subnetwork-1"
+      ip_cidr_range            = "10.10.0.0/24"
+      secondary_ip_range       = null
+      private_ip_google_access = null
+      log_config               = null
+    }
+  ]
+}
+
 module "sql" {
-  source           = "./modules/sql"
-  instance_name    = "example-sql"
+  source           = "../../modules/sql"
+  name             = "example-sql"
   database_type    = "mysql"
   database_version = "5.7"
   ssl_common_name  = "example"
 
-  ipv4_enabled = true
+  is_private = true
+
+  ip_configuration = {
+    ipv4_enabled        = false
+    private_network     = module.vpc.id
+    require_ssl         = null
+    authorized_networks = null
+  }
 
   users = [
     {
